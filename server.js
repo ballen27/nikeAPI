f2ce71e5a26f8b4cc9867cbaf1523fdd11507f71
Bronson Allen <ballen27@byu.edu>
2017-08-03 17:53:41 -0600

    updated comments

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ var server = app.listen(port, function() {
     console.log('Express server listening on port ' + port);
 });
 
-
 /*
 Notes on callbacks vs. promises
 =========================================================================
